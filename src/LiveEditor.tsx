2dd7dc4f5d2aeb4f7cd723bd8bcf20262a302870
Joey <mycityofsky@gmail.com>
2022-05-30 11:12:25 +0800

    use div in LiveEditor

--- src/LiveEditor.tsx
+++ src/LiveEditor.tsx
@@ -1,19 +1,22 @@
 import { useState } from "react"
+import Heading from "./components/Heading"
 
 export default function LiveEditor() {
 
   const [input, setInput] = useState("")
 
     return (
-        <textarea value={input}
-               onChange={e => setInput(e.target.value)}
+        <div contentEditable="true"
                style={{
                    width: "100%",
                    height: "100%",
                    position: "absolute",
                    top: 0,
                    left: 0,
-               }}/>
+               }}>
+
+            <Heading text="Test" />
+        </div>
     )
 
 }
