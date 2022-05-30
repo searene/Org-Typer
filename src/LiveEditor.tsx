af49c6d6f5abf6d149548bb1b9ea1cbc867110b5
Joey <mycityofsky@gmail.com>
2022-05-30 11:19:13 +0800

    add handleBlur

--- src/LiveEditor.tsx
+++ src/LiveEditor.tsx
@@ -3,17 +3,23 @@ import Heading from "./components/Heading"
 
 export default function LiveEditor() {
 
-  const [input, setInput] = useState("")
+    const [input, setInput] = useState("")
+
+    const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
+        setInput(e.target.value);
+    }
 
     return (
         <div contentEditable="true"
-               style={{
-                   width: "100%",
-                   height: "100%",
-                   position: "absolute",
-                   top: 0,
-                   left: 0,
-               }}>
+            suppressContentEditableWarning={true}
+            onBlur={handleBlur}
+            style={{
+                width: "100%",
+                height: "100%",
+                position: "absolute",
+                top: 0,
+                left: 0,
+            }}>
 
             <Heading text="Test" />
         </div>
