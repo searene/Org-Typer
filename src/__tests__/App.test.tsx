d92d05bb0ae05f4555befff32eee30046ea0d9e0
Joey <mycityofsky@gmail.com>
2022-05-30 09:02:36 +0800

    Add a react test.

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,7 +1,14 @@
+import {render, screen} from '@testing-library/react';
 import { expect, describe, it } from 'vitest'
+import App from '../App';
 
 describe("App Test", () => {
   it("1 + 1 should equal 2", () => {
       expect(1 + 1).toEqual(2);
   });
+
+  it("should show title", () => {
+    render(<App />);
+    expect(screen.getByText(/Test/)).toBeDefined()
+  })
 });
