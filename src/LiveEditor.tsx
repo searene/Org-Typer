65d80cf99742b090b533d515bba5984975dfbfd6
Joey <mycityofsky@gmail.com>
2022-05-30 10:56:07 +0800

    add text editor

--- src/LiveEditor.tsx
+++ src/LiveEditor.tsx
@@ -1,7 +1,19 @@
+import { useState } from "react"
+
 export default function LiveEditor() {
 
+  const [input, setInput] = useState("")
+
     return (
-        <div>Test</div>
+        <textarea value={input}
+               onChange={e => setInput(e.target.value)}
+               style={{
+                   width: "100%",
+                   height: "100%",
+                   position: "absolute",
+                   top: 0,
+                   left: 0,
+               }}/>
     )
 
 }
