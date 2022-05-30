d80b0bd81ec9e70f6791d9c0d8a765d047ef502a
Joey <mycityofsky@gmail.com>
2022-05-30 10:49:51 +0800

    add LiveEditor

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import LiveEditor from './LiveEditor'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>Test</div>
+      <LiveEditor />
     </div>
   )
 }
---
+++ src/LiveEditor.tsx
@@ -0,0 +1,7 @@
+export default function LiveEditor() {
+
+    return (
+        <div>Test</div>
+    )
+
+}
