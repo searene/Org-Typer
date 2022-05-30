d9ddb52735f6bb1bca203519e9356779218a4b41
Joey <mycityofsky@gmail.com>
2022-05-30 13:36:29 +0800

    add MarkdownPreviewExample

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import LiveEditor from './editor/LiveEditor'
+import MarkdownPreviewExample from './MarkdownPreviewExample'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <LiveEditor />
+      {/* <LiveEditor /> */}
+      <MarkdownPreviewExample/>
     </div>
   )
 }
