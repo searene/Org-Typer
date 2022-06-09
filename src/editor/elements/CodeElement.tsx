c00e05660b96428afa7a9acea0bf443b6d2a8b2c
Searene <mycityofsky@gmail.com>
2022-06-09 09:47:16 +0800

    add code mirror, but didn't work

--- src/editor/elements/CodeElement.tsx
+++ src/editor/elements/CodeElement.tsx
@@ -1,4 +1,6 @@
 import CustomText from "../CustomText"
+import CodeMirror from '@uiw/react-codemirror';
+import { javascript } from '@codemirror/lang-javascript';
 
 export type CodeBlockElementType = {
     type: 'codeBlock'
@@ -7,10 +9,16 @@ export type CodeBlockElementType = {
 
 export const CodeBlockElement = (props: any) => {
     return (
-      <div>
-        <pre {...props.attributes}>
-          <code>{props.children}</code>
-        </pre>
-      </div>
-    )
+      <CodeMirror
+        {...props.attributes}
+        value="console.log('hello world!');"
+        height="200px"
+        extensions={[javascript({ jsx: true })]}
+        onChange={(value, viewUpdate) => {
+          console.log('value:', value);
+        }}
+      >
+        {props.children}
+      </CodeMirror>
+    );
 }
