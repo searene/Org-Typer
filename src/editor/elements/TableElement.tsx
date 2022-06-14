8663cb4b6e5844618b3ad0cdd4018fdc23eca4c7
Searene <mycityofsky@gmail.com>
2022-06-14 22:25:40 +0800

    add table button

--- src/editor/elements/TableElement.tsx
+++ src/editor/elements/TableElement.tsx
@@ -20,19 +20,37 @@ export const TableElement = (props: RenderElementProps) => {
 
     const getReactNode = (props: RenderElementProps): JSX.Element => {
         if (props.element.type === 'table') {
-            return <table style={{
-                borderCollapse: 'collapse',
-                border: "1px solid black",
-            }}>
-                <tbody {...props.attributes}>{props.children}</tbody>
-            </table>
+            return (
+                <div style={{
+                    width: "100%",
+                }}>
+                    <table style={{
+                        borderCollapse: 'collapse',
+                        width: "100%",
+                    }}>
+                        <tbody {...props.attributes}>{props.children}</tbody>
+                    </table>
+                </div>
+            )
         } else if (props.element.type === 'tableRow') {
             return <tr {...props.attributes}>{props.children}</tr>
         } else if (props.element.type === 'tableCell') {
             return <td {...props.attributes} style={{
-                border: "1px solid black",
+                border: "1px solid gray",
                 padding: "10px",
-            }}>{props.children}</td>
+                position: "relative",
+            }}>
+                <button style={{
+                    position: "absolute",
+                    left: "50%",
+                    top: "0",
+                    transform: "translate(-50%, -50%)",
+                    border: "1px solid black",
+                    textAlign: "right",
+                    visibility: "hidden",
+                }}>test</button>
+                <span>{props.children}</span>
+            </td>
         } else {
             throw new Error(`Unknown element type: ${props.element.type}`)
         }
