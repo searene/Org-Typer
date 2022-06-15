517afdb4f8d7976f270aeee425531f9d11a13d86
Searene <mycityofsky@gmail.com>
2022-06-15 23:15:12 +0800

    show context menus

--- src/editor/elements/TableElement.tsx
+++ src/editor/elements/TableElement.tsx
@@ -1,130 +1,159 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGear } from '@fortawesome/free-solid-svg-icons'
-import { useEffect } from "react"
+import { MouseEventHandler, useEffect } from "react"
 import { RenderElementProps } from "slate-react"
 import CustomText from "../CustomText"
 import { TableCellPosition } from "../leaf/TableCellPosition"
 import { TableUtils } from "../table/TableUtils"
+import { Menu, Item, Separator, Submenu, useContextMenu, TriggerEvent, ItemParams } from "react-contexify";
+import "react-contexify/dist/ReactContexify.css";
 
 export type TableElementType = {
-    type: 'table',
-    children: TableRowElementType[],
+  type: 'table',
+  children: TableRowElementType[],
 }
 
 export type TableRowElementType = {
-    type: 'tableRow',
-    children: TableCellElementType[],
+  type: 'tableRow',
+  children: TableCellElementType[],
 }
 
 export type TableCellElementType = {
-    type: 'tableCell',
-    children: CustomText[],
+  type: 'tableCell',
+  children: CustomText[],
 }
 
 export const TableElement = (props: RenderElementProps) => {
 
-    const TYPER_TD_CONFIG_TOP = "typer-td-config-top"
+  const TYPER_TD_CONFIG_TOP = "typer-td-config-top"
+  const TYPER_TD_CONFIG_LEFT = "typer-td-config-left"
+  const TOP_MENU_ID = "table-top-menu";
 
-    const TYPER_TD_CONFIG_LEFT = "typer-td-config-left"
+  const { show } = useContextMenu({ id: TOP_MENU_ID });
 
-    const getTopConfigureChild = (cell: HTMLTableCellElement): HTMLElement => {
-        for (const child of cell.children) {
-            if (child.className === TYPER_TD_CONFIG_TOP) {
-                return child as HTMLElement
-            }
-        }
-        throw new Error("Didn't find the top configure child")
+  function handleItemClick({ event, props, triggerEvent, data }: ItemParams) {
+    console.log(event, props, triggerEvent, data);
+  }
+
+  function displayMenu(e: TriggerEvent) {
+    // put whatever custom logic you need
+    // you can even decide to not display the Menu
+    show(e);
+  }
+
+  const getTopConfigureChild = (cell: HTMLTableCellElement): HTMLElement => {
+    for (const child of cell.children) {
+      if (child.className === TYPER_TD_CONFIG_TOP) {
+        return child as HTMLElement
+      }
     }
+    throw new Error("Didn't find the top configure child")
+  }
 
-    const getLeftConfigureChild = (cell: HTMLTableCellElement): HTMLElement => {
-        for (const child of cell.children) {
-            if (child.className === TYPER_TD_CONFIG_LEFT) {
-                return child as HTMLElement
-            }
-        }
+  const getLeftConfigureChild = (cell: HTMLTableCellElement): HTMLElement => {
+    for (const child of cell.children) {
+      if (child.className === TYPER_TD_CONFIG_LEFT) {
+        return child as HTMLElement
+      }
     }
+    throw new Error("Didn't find the left configure child")
+  }
 
-    useEffect(() => {
-        const tableCells = document.getElementsByTagName("td")
-        const positions = TableUtils.getPositions(tableCells)
-        for (const cell of tableCells) {
-            cell.addEventListener("mouseover", () => {
-                const pos = TableUtils.getPosition(cell)
-                const cellInFirstRow = positions.get(new TableCellPosition(0, pos.columnIndex).toString())!
-                const cellInFirstColumn = positions.get(new TableCellPosition(pos.rowIndex, 0).toString())!
-                const topConfigureTag = getTopConfigureChild(cellInFirstRow)
-                const leftConfigureTag = getLeftConfigureChild(cellInFirstColumn)
-                topConfigureTag.style.visibility = "visible"
-                leftConfigureTag.style.visibility = "visible"
-            })
-            cell.addEventListener("mouseleave", () => {
-                const pos = TableUtils.getPosition(cell)
-                const cellInFirstRow = positions.get(new TableCellPosition(0, pos.columnIndex).toString())!
-                const cellInFirstColumn = positions.get(new TableCellPosition(pos.rowIndex, 0).toString())!
-                const topConfigureTag = getTopConfigureChild(cellInFirstRow)
-                const leftConfigureTag = getLeftConfigureChild(cellInFirstColumn)
-                topConfigureTag.style.visibility = "hidden"
-                leftConfigureTag.style.visibility = "hidden"
-            })
-        }
-    });
-
-    const getReactNode = (props: RenderElementProps): JSX.Element => {
-        if (props.element.type === 'table') {
-            return (
-                <div style={{
-                    width: "100%",
-                }}>
-                    <table style={{
-                        borderCollapse: 'collapse',
-                        width: "100%",
-                    }}>
-                        <tbody {...props.attributes}>{props.children}</tbody>
-                    </table>
-                </div>
-            )
-        } else if (props.element.type === 'tableRow') {
-            return <tr {...props.attributes}>{props.children}</tr>
-        } else if (props.element.type === 'tableCell') {
-            return <td {...props.attributes} style={{
-                border: "1px solid gray",
-                padding: "10px",
-                position: "relative",
-            }}>
-                <div contentEditable={false} className={TYPER_TD_CONFIG_TOP} style={{
-                    position: "absolute",
-                    left: "50%",
-                    top: "0",
-                    transform: "translate(-50%, -50%)",
-                    border: "1px dashed black",
-                    width: "50px",
-                    textAlign: "center",
-                    visibility: "hidden",
-                    backgroundColor: "white",
-                }}>
-                    <FontAwesomeIcon icon={faGear} />
-                </div>
-                <div className={TYPER_TD_CONFIG_LEFT} style={{
-                    position: "absolute",
-                    left : "0",
-                    top: "50%",
-                    transform: "translate(-50%, -50%)",
-                    border: "1px dashed black",
-                    margin: "auto",
-                    visibility: "hidden",
-                    backgroundColor: "white",
-                }}>
-                    <FontAwesomeIcon icon={faGear} style={{
-                        paddingTop: "5px",
-                        paddingBottom: "5px",
-                    }}/>
-                </div>
-                <span>{props.children}</span>
-            </td>
-        } else {
-            throw new Error(`Unknown element type: ${props.element.type}`)
-        }
+  useEffect(() => {
+    const tableCells = document.getElementsByTagName("td")
+    const positions = TableUtils.getPositions(tableCells)
+    for (const cell of tableCells) {
+      cell.addEventListener("mouseover", () => {
+        const pos = TableUtils.getPosition(cell)
+        const cellInFirstRow = positions.get(new TableCellPosition(0, pos.columnIndex).toString())!
+        const cellInFirstColumn = positions.get(new TableCellPosition(pos.rowIndex, 0).toString())!
+        const topConfigureTag = getTopConfigureChild(cellInFirstRow)
+        const leftConfigureTag = getLeftConfigureChild(cellInFirstColumn)
+        topConfigureTag.style.visibility = "visible"
+        leftConfigureTag.style.visibility = "visible"
+      })
+      cell.addEventListener("mouseleave", () => {
+        const pos = TableUtils.getPosition(cell)
+        const cellInFirstRow = positions.get(new TableCellPosition(0, pos.columnIndex).toString())!
+        const cellInFirstColumn = positions.get(new TableCellPosition(pos.rowIndex, 0).toString())!
+        const topConfigureTag = getTopConfigureChild(cellInFirstRow)
+        const leftConfigureTag = getLeftConfigureChild(cellInFirstColumn)
+        topConfigureTag.style.visibility = "hidden"
+        leftConfigureTag.style.visibility = "hidden"
+      })
     }
+  });
 
-    return getReactNode(props);
+  const getReactNode = (props: RenderElementProps): JSX.Element => {
+    if (props.element.type === 'table') {
+      return (
+        <div style={{
+          width: "100%",
+        }}>
+          <table style={{
+            borderCollapse: 'collapse',
+            width: "100%",
+          }}>
+            <tbody {...props.attributes}>{props.children}</tbody>
+          </table>
+          <Menu id={TOP_MENU_ID}>
+            <Item onClick={handleItemClick}>Item 1</Item>
+            <Item onClick={handleItemClick}>Item 2</Item>
+            <Separator />
+            <Item disabled>Disabled</Item>
+            <Separator />
+            <Submenu label="Submenu">
+              <Item onClick={handleItemClick}>Sub Item 1</Item>
+              <Item onClick={handleItemClick}>Sub Item 2</Item>
+            </Submenu>
+          </Menu>
+        </div>
+      )
+    } else if (props.element.type === 'tableRow') {
+      return <tr {...props.attributes}>{props.children}</tr>
+    } else if (props.element.type === 'tableCell') {
+      return <td {...props.attributes} style={{
+        border: "1px solid gray",
+        padding: "10px",
+        position: "relative",
+      }}>
+        <div contentEditable={false}
+          className={TYPER_TD_CONFIG_TOP}
+          onClick={displayMenu}
+          style={{
+            position: "absolute",
+            left: "50%",
+            top: "0",
+            transform: "translate(-50%, -50%)",
+            border: "1px dashed black",
+            width: "50px",
+            textAlign: "center",
+            visibility: "hidden",
+            backgroundColor: "white",
+          }}>
+          <FontAwesomeIcon icon={faGear} />
+        </div>
+        <div className={TYPER_TD_CONFIG_LEFT} style={{
+          position: "absolute",
+          left: "0",
+          top: "50%",
+          transform: "translate(-50%, -50%)",
+          border: "1px dashed black",
+          margin: "auto",
+          visibility: "hidden",
+          backgroundColor: "white",
+        }}>
+          <FontAwesomeIcon icon={faGear} style={{
+            paddingTop: "5px",
+            paddingBottom: "5px",
+          }} />
+        </div>
+        <span>{props.children}</span>
+      </td>
+    } else {
+      throw new Error(`Unknown element type: ${props.element.type}`)
+    }
+  }
+
+  return getReactNode(props);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    test: {
-      globals: true,
-      environment: 'jsdom',
-    }
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
+  define: {
+    'process.env': {}
+  }
 })
