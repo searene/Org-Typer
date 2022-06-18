8d1ac050beabe0c0a2747c73c1a04d36abada401
Searene <mycityofsky@gmail.com>
2022-06-18 09:55:06 +0800

    implemented insert above and below

--- src/editor/elements/TableElement.tsx
+++ src/editor/elements/TableElement.tsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faGear, faArrowLeft, faArrowRight, faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faGear, faArrowLeft, faArrowRight, faTrash, faArrowUp, faArrowDown } from '@fortawesome/free-solid-svg-icons'
 import { MouseEventHandler, useEffect, useRef, useState } from "react"
 import { ReactEditor, RenderElementProps } from "slate-react"
 import CustomText from "../CustomText"
@@ -13,6 +13,11 @@ import Cell from "../../editable-table-example/Cell"
 export enum HorizontalDirection {
   Left,
   Right
+}
+
+export enum VerticalDirection {
+  Up,
+  Down
 }
 
 type ContextMenuProps = {
@@ -39,6 +44,10 @@ enum ContextMenuOperation {
   InsertLeft = "InsertLeft",
   InsertRight = "InsertRight",
   TrashColumn = "TrashColumn",
+  
+  InsertAbove = "InsertAbove",
+  InsertBelow = "InsertBelow",
+  TrashRow = "TrashRow",
 }
 
 export type TableElementProps = RenderElementProps & {
@@ -47,11 +56,13 @@ export type TableElementProps = RenderElementProps & {
 
 export const TableElement = (props: TableElementProps) => {
 
-  const TYPER_TD_CONFIG_TOP = "typer-td-config typer-td-config-top"
-  const TYPER_TD_CONFIG_LEFT = "typer-td-config typer-td-config-left"
-  const TOP_MENU_ID = "table-top-menu";
+  const TYPER_TD_CONFIG_FOR_COLUMN = "typer-td-config typer-td-config-top"
+  const TYPER_TD_CONFIG_FOR_ROW = "typer-td-config typer-td-config-left"
+  const COLUMN_MENU_ID = "table-column-menu"
+  const ROW_MENU_ID = "table-row-menu"
 
-  const { show } = useContextMenu({ id: TOP_MENU_ID });
+  const { show: showColumnMenu } = useContextMenu({ id: COLUMN_MENU_ID });
+  const { show: showRowMenu } = useContextMenu({id: ROW_MENU_ID })
 
   function handleItemClick(params: ItemParams, op: ContextMenuOperation) {
     const contextMenuProps: ContextMenuProps = params.props
@@ -59,6 +70,10 @@ export const TableElement = (props: TableElementProps) => {
       insertColumn(contextMenuProps.tableCellElement, props.editor, contextMenuProps.tableCellNode, HorizontalDirection.Left)
     } else if (op === ContextMenuOperation.InsertRight) {
       insertColumn(contextMenuProps.tableCellElement, props.editor, contextMenuProps.tableCellNode, HorizontalDirection.Right)
+    } else if (op === ContextMenuOperation.InsertAbove) {
+      insertRow(contextMenuProps.tableCellElement, props.editor, contextMenuProps.tableCellNode, VerticalDirection.Up)
+    } else if (op === ContextMenuOperation.InsertBelow) {
+      insertRow(contextMenuProps.tableCellElement, props.editor, contextMenuProps.tableCellNode, VerticalDirection.Down)
     } else {
       throw new Error("Unknown operation: " + op)
     }
@@ -66,7 +81,7 @@ export const TableElement = (props: TableElementProps) => {
 
   const getTopConfigureChild = (cell: HTMLTableCellElement): HTMLElement => {
     for (const child of cell.children) {
-      if (child.className === TYPER_TD_CONFIG_TOP) {
+      if (child.className === TYPER_TD_CONFIG_FOR_COLUMN) {
         return child as HTMLElement
       }
     }
@@ -75,7 +90,7 @@ export const TableElement = (props: TableElementProps) => {
 
   const getLeftConfigureChild = (cell: HTMLTableCellElement): HTMLElement => {
     for (const child of cell.children) {
-      if (child.className === TYPER_TD_CONFIG_LEFT) {
+      if (child.className === TYPER_TD_CONFIG_FOR_ROW) {
         return child as HTMLElement
       }
     }
@@ -92,13 +107,29 @@ export const TableElement = (props: TableElementProps) => {
     for (let i = 0; i < tableLayout.rowCount; i++) {
       Transforms.insertNodes(props.editor, {
         type: 'tableCell',
-        children: [{ text: 'abc' }]
+        children: [{ text: '' }]
       }, {
         at: cellPath,
       })
       cellPath = Path.next(Path.parent(cellPath))
       cellPath.push(currentTableCellPos.columnIndex + (direction === HorizontalDirection.Left ? 0 : 1))
     }
+  }
+
+  const insertRow = (currentTableCell: HTMLTableCellElement, editor: Editor, slateTableCellNode: Node, direction: VerticalDirection) => {
+    const tableLayout = TableUtils.getTableLayout(currentTableCell)
+    const cellPath = ReactEditor.findPath(editor, slateTableCellNode)
+    let rowPath = Path.parent(cellPath)
+    const children: TableCellElementType[] = []
+    for (let i = 0; i < tableLayout.columnCount; i++) {
+      children.push({ type: 'tableCell', children: [{ text: '' }] })
+    }
+    Transforms.insertNodes(props.editor, {
+      type: 'tableRow',
+      children: children,
+    }, {
+      at: direction === VerticalDirection.Up ? rowPath : Path.next(rowPath),
+    })
   }
 
   const cellMouseoverEventListener = (cell: HTMLTableCellElement, posToCellMap: Map<string, HTMLTableCellElement>) => {
@@ -137,7 +168,6 @@ export const TableElement = (props: TableElementProps) => {
     }
     return () => {
       for (const cell of tableCells) {
-        const mouseoverEventListner = cellToMouseoverEventListenerMap.get(cell)!
         cell.removeEventListener("mouseover", cellToMouseoverEventListenerMap.get(cell)!)
         cell.removeEventListener("mouseleave", cellToMouseleaveEventListenerMap.get(cell)!)
       }
@@ -157,7 +187,7 @@ export const TableElement = (props: TableElementProps) => {
             <tbody {...props.attributes}>{props.children}</tbody>
           </table>
           {/* TODO We will have multiple Menus with the same id if we have multiple tables, need to fix it */}
-          <Menu id={TOP_MENU_ID} contentEditable={false}>
+          <Menu id={COLUMN_MENU_ID} contentEditable={false}>
             <Item onClick={(params: ItemParams) => handleItemClick(params, ContextMenuOperation.InsertLeft)}>
               <FontAwesomeIcon icon={faArrowLeft} style={{paddingRight: "10px"}}/>
               Insert Left
@@ -171,6 +201,20 @@ export const TableElement = (props: TableElementProps) => {
               Trash
             </Item>
           </Menu>
+          <Menu id={ROW_MENU_ID} contentEditable={false}>
+            <Item onClick={(params: ItemParams) => handleItemClick(params, ContextMenuOperation.InsertAbove)}>
+              <FontAwesomeIcon icon={faArrowUp} style={{paddingRight: "10px"}}/>
+              Insert Above
+            </Item>
+            <Item onClick={(params: ItemParams) => handleItemClick(params, ContextMenuOperation.InsertBelow)}>
+              <FontAwesomeIcon icon={faArrowDown} style={{paddingRight: "10px"}}/>
+              Insert Below
+            </Item>
+            <Item onClick={(params: ItemParams) => handleItemClick(params, ContextMenuOperation.TrashRow)}>
+              <FontAwesomeIcon icon={faTrash} style={{paddingRight: "10px"}}/>
+              Trash The Current Row
+            </Item>
+          </Menu>
         </div>
       )
     } else if (props.element.type === 'tableRow') {
@@ -182,13 +226,13 @@ export const TableElement = (props: TableElementProps) => {
         position: "relative",
       }}>
         <div contentEditable={false}
-          className={TYPER_TD_CONFIG_TOP}
+          className={TYPER_TD_CONFIG_FOR_COLUMN}
           onClick={(e: React.MouseEvent<HTMLDivElement>) => {
             const contextMenuProps: ContextMenuProps = {
               tableCellElement: e.currentTarget.parentElement as HTMLTableCellElement,
               tableCellNode: props.element,
             }
-            show(e, { props: contextMenuProps })
+            showColumnMenu(e, { props: contextMenuProps })
           }}
           style={{
             position: "absolute",
@@ -203,16 +247,24 @@ export const TableElement = (props: TableElementProps) => {
           }}>
           <FontAwesomeIcon icon={faGear} />
         </div>
-        <div contentEditable={false} className={TYPER_TD_CONFIG_LEFT} style={{
-          position: "absolute",
-          left: "0",
-          top: "50%",
-          transform: "translate(-50%, -50%)",
-          border: "1px dashed black",
-          margin: "auto",
-          visibility: "hidden",
-          backgroundColor: "white",
-        }}>
+        <div contentEditable={false} className={TYPER_TD_CONFIG_FOR_ROW} 
+          onClick={(e: React.MouseEvent<HTMLDivElement>) => {
+            const contextMenuProps: ContextMenuProps = {
+              tableCellElement: e.currentTarget.parentElement as HTMLTableCellElement,
+              tableCellNode: props.element,
+            }
+            showRowMenu(e, { props: contextMenuProps })
+          }}
+          style={{
+            position: "absolute",
+            left: "0",
+            top: "50%",
+            transform: "translate(-50%, -50%)",
+            border: "1px dashed black",
+            margin: "auto",
+            visibility: "hidden",
+            backgroundColor: "white",
+          }}>
           <FontAwesomeIcon icon={faGear} style={{
             paddingTop: "5px",
             paddingBottom: "5px",
